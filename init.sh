37fb41a2a4ca2a3c94d7070ee18327d1fbc2e442
Jokin <jokinaizpuru28@gmail.com>
2024-11-21 10:31:04 +0100

    Fix init.sh, remove code script lines

--- init.sh
+++ init.sh
@@ -6,9 +6,8 @@ python3 -m venv .venv
 # Activate the virtual environment
 source .venv/bin/activate
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install the required packages
 pip install -r requirements.txt
-
-# Select the interpreter in VSCode
-code --install-extension ms-python.python
-code -r . --command "python.setInterpreter" --args "$(pwd)/.venv/bin/python"
